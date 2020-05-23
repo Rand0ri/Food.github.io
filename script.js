46f5390a9da06f1c7cee0e38b2ba1ac56cac6c63
Rand0ri <64183606+Rand0ri@users.noreply.github.com>
2020-05-23 12:39:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-const TypeWriter = function(txtElement, words, wait = 3000) { // Take in span element, words from array, amount it wait before it stars deleting. Big first letter because of const
-    this.txtElement = txtElement;  // If we have constructive fucntion we can assign properties using this keyword
+const TypeWriter = function(txtElement, words, wait = 3000) { 
+    this.txtElement = txtElement; 
     this.words = words; 
-    this.txt = ''; // Represents whatever in this area
-    this.wordIndex = 0; // Which word we are on ( array )
-    this.wait = parseInt(wait, 10); // whatever passed as argument of our function. We need make sure it is an integer so we use JS function
-    this.type(); // We need this main method which associates with this typewriter which does everything
-    this.isDeleting = false; // Represents the state if its deleting or not
-
+    this.txt = ''; 
+    this.wordIndex = 0; 
+    this.wait = parseInt(wait, 10); 
+    this.type(); 
+    this.isDeleting = false; 
 }
 
-// Type Method. To add method we need to use prototype
+// Type Method
 TypeWriter.prototype.type = function () {
     // Get the current index of word
-    const current = this.wordIndex % this.words.length; // Use module operator to get total words of the array
+    const current = this.wordIndex % this.words.length; 
     // Get the full text of current word
-    const fullTxt = this.words[current]; // Current index of the word in array
+    const fullTxt = this.words[current]; 
 
     // Check if deleting
-    if (this.isDeleting) { // Checking this property. By defaul false
+    if (this.isDeleting) { 
         // Remove character
         this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
         // Add character
-        // Whatever currently in the span
+       
         this.txt = fullTxt.substring(0, this.txt.length + 1);// Whatever the length is we are adding 1 to it
     }
 
-    // Insert txt into element. (Output whatever in this.txt)
+    
     // Insert span into our html span
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`; // Backticks for template literal
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`; 
    
-    setTimeout(() => this.type(), 200); // Call this method and how often 
+    setTimeout(() => this.type(), 200); 
 }
 
-// Init on DOM Load . (Initializing)
+// Init on DOM Load .
 document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
-    // We need to grab the whole element(span) and attributes
+ 
     const txtElement = document.querySelector(".txt-type");
-    // Method for Parsing
-    const words = JSON.parse(txtElement.getAttribute("data-words")); // We need to parse it so we can use it in our JS otherwise it will be string 
+
+    const words = JSON.parse(txtElement.getAttribute("data-words")); 
     const wait = txtElement.getAttribute("data-wait");
     //Init TypeWriter
     new TypeWriter(txtElement, words, wait);
